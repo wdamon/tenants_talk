3dcf559135e65b378c3029bf720cae31905a6900
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-01-31 14:01:10 -0800

    Yet more static file fuckery

--- antieviction/settings.py
+++ antieviction/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'report/static'),
 )
 
 # Simplified static file serving.
