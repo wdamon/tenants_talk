9a5511bfaa713353554a249d8e93725a9451d74f
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-02-12 11:42:34 -0800

    report arb updates

--- report/static/helper.js
+++ report/static/helper.js
@@ -2,6 +2,7 @@ var formgroup = '<div class="form-group"><label class="control-label" for="%id%"
 var jscript1 = 'var date = $("%id%").val();';
 var jscript2 =  '$("%printid%").append(date);';
 
+var sections = [repintroduction, repbackground, evidence, repconclusion];
 
 var repairsdict = {
   "tname":{
@@ -64,76 +65,90 @@ function textareafy(fgroup, col, row) {
 
 }
 
-var autoformintro = {
+function populateform(dictionary) {
+  for (var fgroup in dictionary) {
+    dictionary[fgroup]["var"] = $("#"+dictionary[fgroup]["id"]).val();
+    $("." + dictionary[fgroup]["id"]+"print").each(function(){
+      $(this).text(function(){
+        return $(this).text().replace(/%text%/, dictionary[fgroup]["var"]);
+      });
+    });
+  }
+}
+
+
+
+var repintroduction = {
   "tdate":{
     "id":"tdate",
     "name":"Todays date",
     "pholder":"MM/DD/YYYY"},
-    "tname":{
+  "tname":{
       "id":"tname",
       "name":"Your Name",
       "pholder":"Your First and Last Name"},
-    "address":{
+  "address":{
       "id":"taddress",
       "name":"Your Address",
        "pholder":"123 Main Street"},
-    "city":{
+  "city":{
       "id":"tcity",
       "name":"Your City of Residence",
       "pholder":"Cityville"},
-    "postal":{
+  "postal":{
       "id":"tpostal",
       "name": "Your Postal Code",
       "pholder":"V5Y1J2"},
-    "lname":{
+  "lname":{
       "id": "lname",
       "name": "Your Landlords Name",
       "pholder": "Jane Doe"},
-    "laddress":{
+  "laddress":{
       "id": "laddress",
       "name": "You Landlords Address",
-      "pholder": "123 Main Street"
-    },
-    "lcity":{
+      "pholder": "123 Main Street"},
+  "lcity":{
       "id":"lcity",
       "name":"Your Landlords City of Residence",
-      "pholder":"Cityville"
-    },
-    "lpostal":{
+      "pholder":"Cityville"},
+  "lpostal":{
       "id":"lpostal",
       "name": "Your Postal Code",
-      "pholder":"V5Y1J2"
-    },
-    "idate":{
+      "pholder":"V5Y1J2"},
+  "idate":{
       "id":"idate",
       "name": "Date the issue started",
-      "pholder": "MM/DD/YYYY"
-    },
-    "issuesummaryprint":{
+      "pholder": "MM/DD/YYYY"},
+  "issuesummaryprint":{
       "id":"issuesummaryprint",
       "name":"Issue summary",
-      "pholder": "Brief summary of the issue, in complete sentences. The sink was broken."
-    },
-    "ldate":{
+      "pholder": "Brief summary of the issue, in complete sentences. The sink was broken."},
+  "ldate":{
       "id":"ldate",
       "name": "Date letter was sent to landlord",
-      "pholder": "MM/DD/YYYY"
-    },
-    "ddate":{
+      "pholder": "MM/DD/YYYY"},
+  "ddate":{
       "id":"ddate",
       "name": "Deadline Given Landlord for Completion of Repairs",
-      "pholder": "MM/DD/YYYY"}
-    }
-
-var autoformbckg =
-  {"repairs":{
-    "id": "repairs",
-    "name":"Description of Repairs",
-    "pholder":"Descriptive List of Repairs. Please use complete sentences. For instance, 'The upstairs sink is leaking.'"
-  },}
-
-</div>
-div id="autoformevd"></div>
-<div id="evidenceblck">
-data-target="#13evd" - hit button and adds another evdblock with updated ID tags
-<div id="autoformconc"></div>
+      "pholder": "MM/DD/YYYY"},
+  "repairs":{
+      "id": "repairs",
+      "name":"Description of Repairs",
+      "pholder":"Descriptive List of Repairs. Please use complete sentences. For instance, 'The upstairs sink is leaking.'"}
+}
+var repbackground = {
+  "bckground":{
+    "id": "bckground",
+    "name":"Background",
+    "pholder":"Provide any additional background you'd like to add to this complaint. You can leave this blank. Be specific and give dates. Less is usually more. "},
+  "impact":{
+    "id":"impact",
+    "name":"Impact",
+    "pholder":"In a few short sentences describe how your landlord's neglegence has negatively effected you. These impacts can be physical, mental, or financial. Be as specific as possible and give dates and full names of witnesses wherever possible."},
+}
+var evidence = {
+  "edate"{
+    "id": "edate",
+    "name":"Evidence Time Stap",
+    "pholder":""},
+}
