97efd0da018a5fc6a0459bd3d9187d112525dc87
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-02-16 15:38:31 -0800

    repair_arb changes extensive

--- report/static/helper.js
+++ report/static/helper.js
@@ -1,8 +1,7 @@
 var formgroup = '<div class="form-group"><label class="control-label" for="%id%">%name%</label> <input class="form-control" id="%id%" name="%name%" placeholder="%pholder%" title="" type="text" required /></div>';
-var jscript1 = 'var date = $("%id%").val();';
-var jscript2 =  '$("%printid%").append(date);';
 
-var evidenceblock = '<div id="eblock%ecount%"><div class="panel panel-default" style="border:1px solid black; font-family:Work Sans"><div class="panel-heading" role="tab" id="heading%ecount%"><h4><span>Evidence #%ecount%</span><button type="button"class="btn btn-default pull-right" data-toggle="collapse" data-target="#bodyid%ecount%" aria-expanded="false" aria-controls="Minimize"><i class="fa fa-minus" aria-hidden="true"></i></button></h4></div><div id="#bodyid%ecount%" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading%ecount%"><div class="panel-body" style="padding-left: 2em; font-family:Work Sans; font-size: 21px; font-weight: 200"><div id="evdform%ecount%"></div></div><button type="button"class="btn btn-default pull-right" id="#btnEvd" aria-expanded="false" aria-controls="addMore"><i class="fa fa-plus-circle" aria-hidden="true">Add more evidence...</i></button></div></div></div>'
+
+var evidenceblock = '<div id="eblock%ecount%"><div class="panel panel-default" style="border:1px solid black; font-family:Work Sans"><div class="panel-heading" role="tab" id="heading%ecount%"><h4><span>Evidence #%ecount%</span><button type="button"class="btn btn-default pull-right" data-toggle="collapse" data-target="#bodyid%ecount%" aria-expanded="false" aria-controls="Minimize"><i class="fa fa-minus" aria-hidden="true"></i></button></h4></div><div id="#bodyid%ecount%" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading%ecount%"><div class="panel-body" style="padding-left: 2em; font-family:Work Sans; font-size: 21px; font-weight: 200"><div id="evdform%ecount%"></div></div><button type="button" class="btn btn-lg btn-danger removeEvd" aria-expanded="false" aria-controls="remove evidence"><i class="fa fa-window-close fa-lg" aria-hidden="true"></i> Remove evidence</button> <button type="button"class="btn btn-lg btn-primary btnEvd" aria-expanded="false" aria-controls="addMore"><i class="fa fa-plus-circle fa-lg" aria-hidden="true"></i> Add more evidence</button></div></div></div>'
 var repairsdict = {
   "tname":{
     "id":"tname",
@@ -71,6 +70,7 @@ function saveform(dictionary) {
       $(this).text(function(){
         return $(this).text().replace(/%text%/, dictionary[fgroup]["var"]);
       });
+
     });
   }
 };
@@ -79,14 +79,23 @@ function printform(num, dict) {
   for (var fgroup in dict) {
     formattedgroup = formgroup.replace(/%name%/g, dict[fgroup]["name"]).replace(/%id%/g, dict[fgroup]["id"]).replace(/%pholder%/g, dict[fgroup]["pholder"]);
     $("#auto"+forms[num]).append(formattedgroup);
-    if (textarealist.indexOf(fgroup) > -1) {
-      textareafy(fgroup, 4, 4);
+    if (dict[fgroup]["widget"] === "textarea") {
+      textareafy(dict[fgroup], 40, 4)
+    } else if (dict[fgroup]["widget"] === "date") {
+      $(dict[fgroup]["id"]).datetimepicker({
+           format: 'YYYY-MM-DD'
+      })
     }
   };
 }
 
 
 var repintroduction = {
+  "date1":{
+      "id":"date1",
+      "name":"Todays Date",
+      "pholder":"YYYY-MM-DD",
+      "widget:":"date"},
   "tname":{
       "id":"tname",
       "name":"Your Name",
@@ -117,40 +126,47 @@ var repintroduction = {
       "pholder":"Cityville"},
   "lpostal":{
       "id":"lpostal",
-      "name": "Your Postal Code",
+      "name": "Your Landlords Postal Code",
       "pholder":"V5Y1J2"},
   "idate":{
       "id":"idate",
       "name": "Date the issue started",
-      "pholder": "MM/DD/YYYY"},
+      "pholder": "YYYY-MM-DD",
+      "widget": "date"},
   "issuesummary":{
       "id":"issuesummary",
       "name":"Issue summary",
-      "pholder": "Brief summary of the issue, in complete sentences. The sink was broken."},
+      "pholder": "Brief summary of the issue, in complete sentences. The sink was broken.",
+      "widget": "textarea"},
   "ldate":{
       "id":"ldate",
       "name": "Date letter was sent to landlord",
-      "pholder": "MM/DD/YYYY"},
+      "pholder": "MM/DD/YYYY",
+      "widget": "date"},
   "ddate":{
       "id":"ddate",
       "name": "Deadline Given Landlord for Completion of Repairs",
-      "pholder": "MM/DD/YYYY"},
+      "pholder": "MM/DD/YYYY",
+      "widget": "date"},
   };
 var repbackground = {
   "bckground":{
     "id": "bckground",
     "name":"Background",
-    "pholder":"Provide any additional background you'd like to add to this complaint. You can leave this blank. Be specific and give dates. Less is usually more. "},
+    "pholder":"Provide any additional background you'd like to add to this complaint. You can leave this blank. Be specific and give dates. Less is usually more.",
+    "widget":"textarea"},
   "impact":{
     "id":"impact",
     "name":"Impact",
-    "pholder":"In a few short sentences describe how your landlord's neglegence has negatively effected you. These impacts can be physical, mental, or financial. Be as specific as possible and give dates and full names of witnesses wherever possible."}
+    "pholder":"In a few short sentences describe how your landlord's neglegence has negatively effected you. These impacts can be physical, mental, or financial. Be as specific as possible and give dates and full names of witnesses wherever possible.",
+    "widget":"textarea"}
   };
 var evidence = {
   "edate":{
     "id": "edate",
     "name":"Evidence Time Stamp",
-    "pholder":""},
+    "pholder":"YYYY-MM-DD",
+    "widget":"date"},
   "ename":{
     "id":"ename",
     "name":"Name",
@@ -162,7 +178,8 @@ var evidence = {
   "edesc":{
     "id":"edesc",
     "name":"Description",
-    "pholder":"A short description of the evidence that includes its signifcance. E.g A photo of the flooding damage caused by the leaking sink"}
+    "pholder":"A short description of the evidence that includes its signifcance. E.g A photo of the flooding damage caused by the leaking sink",
+    "widget":"textarea"}
 };
 var repconclusion = {
   "damages":{
