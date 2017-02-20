1cdc307d005bc6f0f7a7d2db721478c0375b81a6
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-02-20 13:36:11 -0800

    typo

--- report/static/helper.js
+++ report/static/helper.js
@@ -1,5 +1,5 @@
 var formgroup = '<div class="form-group"><label class="control-label" for="%id%">%name%</label> <input class="form-control" id="%id%" name="%name%" placeholder="%pholder%" title="" type="text" required /></div>';
-var dategroup = '<div class="col-sm-6"><div class="form-group"><div class="input-group date" id="%id%"><input type="text" class="form-control" /><span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span></div></div></div>'
+var dategroup = '<div class="form-group"><div class="input-group date" id="%id%"><input type="text" class="form-control" /><span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span></div></div>'
 var evidenceblock = '<div id="eblock%ecount%"><div class="panel panel-default" style="border:1px solid black; font-family:Work Sans"><div class="panel-heading" role="tab" id="heading%ecount%"><h4><span>Evidence #%ecount%</span><button type="button"class="btn btn-default pull-right" data-toggle="collapse" data-target="#bodyid%ecount%" aria-expanded="false" aria-controls="Minimize"><i class="fa fa-minus" aria-hidden="true"></i></button></h4></div><div id="#bodyid%ecount%" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading%ecount%"><div class="panel-body" style="padding-left: 2em; font-family:Work Sans; font-size: 21px; font-weight: 200"><div id="evdform%ecount%"></div></div><button type="button" class="btn btn-lg btn-danger removeEvd" aria-expanded="false" aria-controls="remove evidence"><i class="fa fa-window-close fa-lg" aria-hidden="true"></i> Remove evidence</button> <button type="button"class="btn btn-lg btn-primary btnEvd" aria-expanded="false" aria-controls="addMore"><i class="fa fa-plus-circle fa-lg" aria-hidden="true"></i> Add more evidence</button></div></div></div>'
 var repairsdict = {
   "tname":{
@@ -61,9 +61,10 @@ function textareafy(fgroup, col, row) {
   });
   $input.after($textarea).remove();
 };
+
 function dateafy(fgroup) {
   $input = $("#"+fgroup["id"])
-  formatteddate = dategroup.replace(/%id%/g, fgroup["id"])
+  formatteddate = dategroup.replace(/%id%/g, fgroup["id"]);
   $input.after(formatteddate).remove();
   $("#"+fgroup["id"]).datetimepicker({format: 'YYYY-MM-DD'});
 };
