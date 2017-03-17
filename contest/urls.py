b32f3ec71cad1c3c63486a551b28a30b1c285ef5
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-03-17 10:40:03 -0700

    arb mapping updates

--- contest/urls.py
+++ contest/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.question, name='question'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/choose/$', views.choose, name='choose'),
+    url(r'^fletter/arb', views.arb, name='arb'),
     url(r'^fletter/(?P<page_slug>[\w]+)/$', views.formletter, name='formletter'),
-    url(r'^landing/$', views.landing, name='landing'),
-    url(r'^fletter/arb', views.arb, name='arb')
+    url(r'^landing/$', views.landing, name='landing')
     ]
