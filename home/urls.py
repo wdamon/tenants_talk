7a8c637d8b26038f80c737f2c95ecf2291f8bc8a
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-04-19 13:53:01 -0700

    Url dispatcher bug

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'', views.home, name='home'),
+	url(r'^$', views.home, name='home'),
 	url(r'^about/$', views.about, name='about'),
 ]
